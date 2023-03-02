9b50baa14a43a1d86d9f837edad960a27008fc68
Jojo3235 <joralmand@gmail.com>
2023-03-02 10:39:14 +0100

    otras cosas

---
+++ menu.py
@@ -0,0 +1,22 @@
+import os 
+import helpers
+import vehiculos as vh
+
+
+def iniciar():
+    while True:
+        helpers.limpiar_pantalla()
+
+        print("========================")
+        print("  Bienvenido al Gestor  ")
+        print("========================")
+        print("[1] Listar los vehículos ")
+        print("[2] Buscar un vehículo   ")
+        print("[3] Añadir un vehículo   ")
+        print("[4] Modificar un vehículo")
+        print("[5] Borrar un vehículo   ")
+        print("[6] Cerrar el Gestor    ")
+        print("========================")
+
+        opcion = input("> ")
+        helpers.limpiar_pantalla()

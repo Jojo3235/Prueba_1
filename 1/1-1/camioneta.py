bc9b207b8546f1c8e5d6de6de930a33be6ced963
Jojo3235 <joralmand@gmail.com>
2023-03-05 21:24:34 +0100

    mas cosas

--- 1/1-1/camioneta.py
+++ 1/1-1/camioneta.py
@@ -2,7 +2,8 @@ import sys
 sys.path.insert(0, "1")
 
 from coche import Coche
-
+import config
+import csv
 
 class Camioneta(Coche):
     
@@ -18,3 +19,59 @@ class Camioneta(Coche):
         diccionario.update({"carga":self.carga})
         return diccionario
     
+class Camionetas():
+    
+    lista = []
+    with open(config.DATABASE_PATH, newline='') as fichero:
+        reader = csv.reader(fichero, delimiter=';')
+        for id, color, ruedas, velocidad, cilindrada in reader:
+            if ruedas == "4":
+                camioneta = Camioneta(color, velocidad, cilindrada)
+                lista.append(camioneta)
+
+    @staticmethod
+    def nuevo(color, velocidad, cilindrada):
+        coche = Coche(color, velocidad, cilindrada)
+        Camionetas.lista.append(coche)
+        Camionetas.guardar()
+        return coche
+
+    @staticmethod
+    def buscar(id):
+        for coche in Camionetas.lista:
+            if coche.id == id:
+                return coche
+
+    @staticmethod
+    def modificar(id, color, velocidad, cilindrada):
+        for indice, coche in enumerate(Camionetas.lista):
+            if coche.id == id:
+                Camionetas.lista[indice].color = color
+                Camionetas.lista[indice].velocidad = velocidad
+                Camionetas.lista[indice].cilindrada = cilindrada
+                Camionetas.guardar()
+                return Camionetas.lista[indice]
+
+    @staticmethod
+    def borrar(id):
+        for indice, coche in enumerate(Camionetas.lista):
+            if coche.id == id:
+                coche = Camionetas.lista.pop(indice)
+                Camionetas.guardar()
+                return coche
+            
+    @staticmethod
+    def guardar():
+        with open(config.DATABASE_PATH, "w", newline='\n') as fichero:
+            writer = csv.writer(fichero, delimiter=";")
+            for coche in Camionetas.lista:
+                writer.writerow([coche.id, coche.color, coche.ruedas, coche.velocidad, coche.cilindrada])
+        
+        Camionetas.lista.clear()
+        with open(config.DATABASE_PATH, newline='') as fichero:
+            reader = csv.reader(fichero, delimiter=';')
+            for id, color, ruedas, velocidad, cilindrada in reader:
+                if ruedas == "4":
+                    coche = Coche(color, velocidad, cilindrada)
+                    coche.id = id
+                    Camionetas.lista.append(coche)

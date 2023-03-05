3ff7661aadc08da901a046a21795910a54ccde7e
Jojo3235 <joralmand@gmail.com>
2023-03-05 01:51:50 +0100

    cosas

---
+++ 1/cosa rara/quad.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(0, "1")
+
+from bicicleta import Bicicleta
+from coche import Coche
+
+
+class Quad(Coche, Bicicleta):
+    def __init__(self, color, tipo, carga, modelo):
+        Bicicleta.__init__(self, color, tipo)
+        Coche.__init__(self, color, 4)
+        self.carga = carga
+        self.modelo = modelo
+
+    def __str__(self):
+        return Bicicleta.__str__(self) + ", {} kg, modelo {}".format(self.carga, self.modelo)
+    
+    def to_dict(self):
+        diccionario = Bicicleta.to_dict(self)
+        diccionario.update(Coche.to_dict(self))
+        diccionario.update({"carga":self.carga, "modelo":self.modelo})
+        return diccionario
